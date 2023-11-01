84da1f94024371212040b4c7e14908d2f1468ef8
MatTayzer <matthieu.cartiere1211@gmail.com>
2023-11-01 12:06:37 +0100

    Save

---
+++ src/main/java/com/mattayzer/fishingwell/FishingWellClient.java
@@ -0,0 +1,10 @@
+package com.mattayzer.fishingwell;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishingWellClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/mattayzer/fishingwell/FishingWellDataGenerator.java
@@ -0,0 +1,11 @@
+package com.mattayzer.fishingwell;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FishingWellDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
